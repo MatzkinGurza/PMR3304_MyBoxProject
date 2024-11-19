1dc9addb241ddbd1e647b22ed8da7429cecf275f
Gustavo <gustavoloioladossantos@gmail.com>
2024-11-19 19:04:31 -0300

    implementar mudanca botoes header para perfil logado, pagina de loja parcialmente feita

--- MyBox/store/urls.py
+++ MyBox/store/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),  # Página do vendedor
-    path('<int:seller_id>/', views.store_page, name='store_page'),
+    path('<int:box_id>/manage/', views.manage_box, name='manage_box'),  # Rota para gerenciar uma Box específica
+    path('add/', views.manage_box, name='manage_box'),  # Rota para adicionar uma nova Box
+    path('<int:seller_id>/', views.store_page, name='store_page'),  # Rota para a página da loja
 ]
--- MyBox/store/views.py
+++ MyBox/store/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .models import Box
+from .forms import BoxForm
 from django.core.paginator import Paginator
 
 @login_required
@@ -22,3 +24,22 @@ def store_page(request, seller_id):
         'boxes': page_obj,
     }
     return render(request, 'store/store_page.html', context)
+
+@login_required
+def manage_box(request, box_id=None):
+    if box_id:
+        box = get_object_or_404(Box, id=box_id, seller=request.user)  # Verifica se o Box pertence ao vendedor
+    else:
+        box = None
+
+    if request.method == 'POST':
+        form = BoxForm(request.POST, request.FILES, instance=box)
+        if form.is_valid():
+            box = form.save(commit=False)
+            box.seller = request.user
+            box.save()
+            return redirect('store:store_page', seller_id=request.user.id)  # Redireciona para a página da loja
+    else:
+        form = BoxForm(instance=box)
+
+    return render(request, 'store/manage_box.html', {'form': form, 'box': box})
