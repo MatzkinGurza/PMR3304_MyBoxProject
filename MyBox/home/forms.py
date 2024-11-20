797928aee9b22a54abff6c22c47f1785be50d04d
MatzkinGurza <gurza.mateus@usp.br>
2024-11-19 21:45:24 -0300

    started addbox and updatebox views

--- MyBox/home/forms.py
+++ MyBox/home/forms.py
@@ -1,10 +1,46 @@
 from django import forms
 from .models import Box
 
+# class BoxForm(forms.ModelForm):
+#     class Meta:
+#         model = Box
+#         fields = ['name', 'tag', 'price', 'description', 'image']
+#         widgets = {
+#             'description': forms.Textarea(attrs={'rows': 4}),
+#         }
+
 class BoxForm(forms.ModelForm):
-    class Meta:
+    image = forms.ImageField(required=False)  # campo para upload de imagem
+
+    class Meta: 
         model = Box
-        fields = ['name', 'tag', 'price', 'description', 'image']
+        fields = ('name', 
+                  'tag', 
+                  'seller', 
+                  'price',
+                  'description' )
         widgets = {
-            'description': forms.Textarea(attrs={'rows': 4}),
+            'name': forms.TextInput(attrs={'class': 'form-control','placeholder':'write your title here'}), 
+            'tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'choose a tag for the post'}), 
+            'seller': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'authorID', 'type': 'hidden'}),
+            'price': forms.FloatField(),
+            'description': forms.Textarea(attrs={'class': 'form-control','placeholder':'write your post here'})
+        }
+
+class BoxFormUpdate(forms.ModelForm):
+    image = forms.ImageField(required=False)  # campo para upload de imagem
+
+    class Meta: 
+        model = Box
+        fields = ('name', 
+                  'tag', 
+                  'seller', 
+                  'price',
+                  'description' )
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control','placeholder':'write your title here'}), 
+            'tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'choose a tag for the post'}), 
+            'seller': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'authorID', 'type': 'hidden'}),
+            'price': forms.FloatField(),
+            'description': forms.Textarea(attrs={'class': 'form-control','placeholder':'write your post here'})
         }
