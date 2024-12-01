4f9f92478e520573df5ea7c32f8a399e310b32f5
Gustavo <gustavoloioladossantos@gmail.com>
2024-12-01 17:24:07 -0300

    arrumar forms de pagamento

--- MyBox/home/forms.py
+++ MyBox/home/forms.py
@@ -8,6 +8,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
+        labels = {
+            'name': 'Nome',
+            'body': 'Comentário',	
+            }
         widgets = {
         'name': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'Comment_authorID', 'type': 'hidden'}), 
         'body': forms.Textarea(attrs={'class': 'form-control'}), 
